48543f8de479bf6902cab76e4453cf9ad7ae6543
Alexander <alexandermakhortov93@gmail.com>
2022-05-14 20:23:06 +0300

    additional practice 8

---
+++ additionalPractice/additionalPractice8.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Место для первой задачи
+let decCache = [],
+    decCases = [2, 0, 1, 1, 1, 2];
+
+function getTimeFromMinutes(time) {
+
+   const titles = ['час', 'часа', 'часов'];
+
+   if (typeof(time) == 'number' && time >= -1 && Number.isInteger(time)){
+      const timeMiliSeconds = time * 60;
+
+      const timeAoures =  Math.floor(timeMiliSeconds / 3600);
+      const timeMinutes = timeMiliSeconds / 60 - timeAoures * 60;
+
+      if(!decCache[timeAoures]) decCache[timeAoures] = timeAoures % 100 > 4 && timeAoures % 100 < 20 ? 2 : decCases[Math.min(timeAoures % 10, 5)];
+
+      return(`Это ${timeAoures} ${titles[decCache[timeAoures]]} и ${timeMinutes} минут`);
+      
+   } else {
+      return('Ошибка, проверьте данные');
+   }
+}
+
+getTimeFromMinutes(100);
+
+// Место для второй задачи
+function findMaxNumber(one, two, three, fore) {
+   
+   const arrNumber = [one, two, three, fore];
+
+   let element = arrNumber[0];
+
+   for (let i = 0; i < arrNumber.length; i++){
+      if (typeof(arrNumber[i]) == 'number'){
+         
+         if (arrNumber[i] >= element){
+            element = arrNumber[i];
+         }
+
+         if(i == arrNumber.length - 1){
+            return element;
+         }
+         
+      } else {
+         return 0;
+      }
+   }
+}
+
+
