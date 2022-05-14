54c3045c94a0689f3c6c9b3273a9bcd090390d3c
Alexander <alexandermakhortov93@gmail.com>
2022-05-14 12:06:41 +0300

    Leson 26

--- js/script.js
+++ js/script.js
@@ -278,3 +278,17 @@
 // };
 
 // logger();
+
+
+
+//// Урок 26
+
+const usdCurr = 28;
+const eurCurr = 32;
+
+function convert(amount, curr) {
+   console.log(curr * amount);
+}
+
+convert(500, usdCurr);
+convert(500, eurCurr);
