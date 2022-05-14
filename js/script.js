624106d0c7cbf96d516191ffaf37d8aa7cbdb010
Alexander <alexandermakhortov93@gmail.com>
2022-05-14 12:01:12 +0300

    Leson 25

--- js/script.js
+++ js/script.js
@@ -242,3 +242,39 @@
 // console.log(result);
 
 
+//// Урок 25
+
+// let num = 20; // Глобальная переменная
+
+// function showFirstMessage(text) {
+//    console.log(text);
+//    let num = 20; //Локальная переменная
+//    num = 10; // Перезапись глобально1 переменно1
+//    console.log(num);
+// }
+
+// showFirstMessage('Hello World!'); // Вызов функции
+// console.log(num);
+
+
+// function calc(a, b){
+//    return (a + b);
+// }
+
+// console.log(calc(5, 3));
+
+
+// function ret(){
+//    let num = 50;
+//    return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//    console.log('Hello');
+// };
+
+// logger();
