b7f63f5ce34e68e91debf602604d76c6b821944a
Alexander <alexandermakhortov93@gmail.com>
2022-06-02 19:59:22 +0300

    40 замыкание и логические окружения

--- js/script.js
+++ js/script.js
@@ -643,3 +643,36 @@
 
 // //3)
 // console.log(typeof(!!'44444'));
+
+
+////Урок 40 Замыкание и лексическое окружение
+
+// let number = 5;
+
+// function logNumber() {
+//    console.log(number);
+// }
+
+// number = 6;
+
+// logNumber();
+
+// function createCounter(){
+//    let counter = 0;
+
+//    const myFunction = function() {
+//       counter = counter + 1;
+//       return counter;
+//    }
+
+//    return myFunction;
+// }
+
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+
+// console.log(c1, c2, c3);
+
+
