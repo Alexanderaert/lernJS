2130e7c78b2e9764ca24a786858bda130872b1f1
Alexander <alexandermakhortov93@gmail.com>
2022-06-05 16:22:40 +0300

    43 использоване методов для взоимоде1ствием с элементами на странице

--- pagesUse/lern 42/js/script.js
+++ pagesUse/lern 42/js/script.js
@@ -1,18 +1,36 @@
 'use strict';
 
 const box = document.getElementById('box'); // Получает элемент по ID
-console.log(box);
-
 const btns = document.getElementsByTagName('button'); // Получаяет элемент по Teg
-console.log(btns[2]);
-
 const circles = document.getElementsByClassName('circle'); // Получает элемент по классу
-console.log(circles);
+const wrapper = document.querySelector('.wrapper');
+const hearts = wrapper.querySelectorAll('.heart'); // получает элемент по селектору
+const oneHeart = wrapper.querySelector('.heart'); // вызывает первы1 элемент
 
-const hearts = document.querySelectorAll('.heart'); // получает элемент по селектору
+
+
+box.style.cssText = 'background-color: blue; width: 400px';
+
 hearts.forEach(item => { // есть возможно использовать форыч
-   console.log(item);
+   item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart'); // вызывает первы1 элемент
-console.log(oneHeart);
+const div = document.createElement('div'); // Создаем элемент
+div.classList.add('black');
+
+wrapper.append(div);// append добавляет в конец блок
+// wrapper.prepend(div); // prepend Добовляет в начало блок
+
+div.innerHTML = '<p>Hello World</p>'; // Добавляет html структуру
+// div.textContent = 'Hello'; // Добавляет текст
+
+// hearts[0].before(div); // Перед первым элементом вставляет блок
+// hearts[0].after(div); // после вставить блок
+
+// circles[0].remove(); // удаляет элемент
+hearts[0].replaceWith(circles[0]); // Заменяет элемент
+
+// div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>'); // Вставляет перед элементом
+// div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>'); // Вставляет во внутрь вначало элемента
+// div.insertAdjacentHTML('beforeend', '<h2>Hello</h2>'); // Вставляет в конец элемента
+// div.insertAdjacentHTML('afterend', '<h2>Hello</h2>'); // Вставляет после элемента
