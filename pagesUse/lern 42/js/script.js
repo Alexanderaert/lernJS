84398c8a728abb943aa9bad77891a6a7b047a76e
Alexander <alexandermakhortov93@gmail.com>
2022-06-05 15:37:20 +0300

    42 урок использование странице

--- pagesUse/lern 42/js/script.js
+++ pagesUse/lern 42/js/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
-aa
+const box = document.getElementById('box'); // Получает элемент по ID
+console.log(box);
+
+const btns = document.getElementsByTagName('button'); // Получаяет элемент по Teg
+console.log(btns[2]);
+
+const circles = document.getElementsByClassName('circle'); // Получает элемент по классу
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart'); // получает элемент по селектору
+hearts.forEach(item => { // есть возможно использовать форыч
+   console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart'); // вызывает первы1 элемент
+console.log(oneHeart);
